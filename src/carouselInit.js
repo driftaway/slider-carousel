59a756127941c6990be7556773c2833743cd1311
unknown <gretomaszek@gmail.com>
2019-04-17 12:12:42 +0200

    updates

---
+++ src/carouselInit.js
@@ -0,0 +1,15 @@
+var glide = new Glide(".glide", {
+    type: "carousel",
+    focusAt: "center",
+    perView: 7,
+    startAt: 1,
+    gap: 30,
+    perTouch: false,
+    animationDuration: 150,
+    dragThreshold: 10,
+    swipeThreshold: 10,
+    touchRatio: 1,
+    touchAngle: 90
+});
+
+glide.mount();
---
+++ src/main.js
@@ -0,0 +1,9 @@
+$('.glide__arrow--right').click(() => {
+    $('.trala').hide()
+    $('.trala').fadeIn(200)
+})
+
+$('.glide__arrow--left').click(() => {
+    $('.trala').hide()
+    $('.trala').fadeIn(200)
+})
