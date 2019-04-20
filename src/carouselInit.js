bd4b020f84606f4a783f40407c6b0edd7354af5a
driftaway <gretomaszek@gmail.com>
2019-04-20 12:05:44 +0200

    update

--- src/carouselInit.js
+++ src/carouselInit.js
@@ -1,15 +1,34 @@
-var glide = new Glide(".glide", {
-    type: "carousel",
-    focusAt: "center",
+let glide = new Glide('.glide', {
+    type: 'carousel',
+    focusAt: 'center',
     perView: 7,
     startAt: 1,
-    gap: 10,
+    gap: 0,
     perTouch: false,
-    animationDuration: 100,
+    animationDuration: 50,
     dragThreshold: 10,
     swipeThreshold: 10,
     touchRatio: 1,
-    touchAngle: 90
-});
+    touchAngle: 90,
+    keyboard: false
+})
 
-glide.mount();
+let glideMobile = new Glide('.glide', {
+    type: 'carousel',
+    focusAt: 'center',
+    perView: 3,
+    startAt: 1,
+    gap: 10,
+    perTouch: false,
+    animationDuration: 50,
+    dragThreshold: 10,
+    swipeThreshold: 10,
+    touchRatio: 1,
+    touchAngle: 90,
+    keyboard: false
+})
+
+let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+if (isMobile == true || window.innerWidth <= 900) {
+    glideMobile.mount()
+} else glide.mount()
--- src/main.js
+++ src/main.js
@@ -1,9 +1,74 @@
-$('.glide__arrow--right').click(() => {
-    $('.trala').hide()
-    $('.trala').fadeIn(200)
-})
+  const $carSection = $('.carSection')
+  const $carBackground = $('.carBackground')
 
-$('.glide__arrow--left').click(() => {
-    $('.trala').hide()
-    $('.trala').fadeIn(200)
-})
+  $carBackground.hide()
+  $carSection.hide()
+
+  function checkIfCarMatches() {
+      setTimeout(() => {
+          let $carName = $('.glide__slide--active').attr('class').split(' ')[1]
+          let $findCarSection = $('.' + $carName + 'Section')
+          if ($findCarSection.is(':visible')) {
+              return false
+          } else {
+              $carSection.hide()
+              $findCarSection.show()
+          }
+      }, 250)
+  }
+
+  function showCurrentCarSection() {
+      setTimeout(() => {
+          $carSection.hide()
+          let $carName = $('.glide__slide--active').attr('class').split(' ')[1]
+          let $findCarSection = $('.' + $carName + 'Section')
+          $findCarSection.show()
+      }, 125)
+  }
+
+
+  $('.glide__arrow--right, .glide__arrow--left').click(() => {
+      $carBackground.hide()
+      $carBackground.fadeIn(250)
+      showCurrentCarSection()
+  })
+
+  $('.iconPrev').click(() => {
+      $('.glide__arrow--left').click()
+  })
+
+  $('.iconNext').click(() => {
+      $('.glide__arrow--right').click()
+  })
+
+  $('.glide__slide').mousedown(() => {
+      $('.glide__slide').css('cursor', 'grabbing')
+  })
+
+  $('.glide__slide').mouseup(() => {
+      $carBackground.hide()
+      $carBackground.fadeIn(250)
+      showCurrentCarSection()
+      checkIfCarMatches()
+      $('.glide__slide').css('cursor', 'grab')
+  })
+
+  $('.glide__slide').touchend(() => {
+      $('.glide__slide--active').click()
+  })
+
+  $('.glide__slide').touchend(() => {
+      $('.glide__slide--active').click()
+  })
+
+  $('.glide__slide').touchstart(() => {
+      $carBackground.hide()
+      $carBackground.fadeIn(250)
+      showCurrentCarSection()
+  })
+
+  $('.glide__slide').touchstart(() => {
+      $carBackground.hide()
+      $carBackground.fadeIn(250)
+      showCurrentCarSection()
+  })
